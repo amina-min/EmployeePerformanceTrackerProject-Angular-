7a98379f578aaded296e90b0824b022575922ab3
Amina <aminaemcbd@gmail.com>
2022-01-10 01:48:46 +0600

    reating save

--- src/app/components/admin/empmanage/empmanage.component.ts
+++ src/app/components/admin/empmanage/empmanage.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { EmployeeRatings } from './empmanageModel';
 
 @Component({
   selector: 'app-empmanage',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empmanage.component.css']
 })
 export class EmpmanageComponent implements OnInit {
+  
+  employeeRatings:EmployeeRatings = new EmployeeRatings()
 
-  constructor() { }
+  constructor( private http:HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {   
+  }
+
+  save() {   
+    
+    console.log(this.employeeRatings.department);
+    const headers = { 'content-Type': 'application/json' };
+    this.http.post<any>("http://localhost:9090/saveEmpRatings", JSON.stringify(this.employeeRatings), { headers: headers })
+      .subscribe(data => {
+        console.log(data);        
+      }
+      ) 
   }
 
 }
